84490a0c69ef059836cdcc3328504ff8fbac96fc
TounsiAhmed <ahmed.tounsi178@gmail.com>
2021-07-23 11:30:00 +0100

    test2

---
+++ contracts/VoteToken.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract VoteToken is ERC20, Ownable {
+
+  constructor() ERC20("VoteToken", "VT")  {
+    _mint(msg.sender, 10000 * 10 ** uint256(0));
+  }
+
+}
